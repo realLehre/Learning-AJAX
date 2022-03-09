49f6c7083d1f25ce53426bf37da6c63f6e1d79a9
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-09 11:06:30 -0600

    update

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const form = document.getElementById('form');
-const input = document.getElementById('input').value;
+
 
 form.addEventListener('submit', fire)
 
 function fire(e){
-    console.log(input);
+    const input = document.getElementById('input').value;
+    
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'random.json', true)
+
+    xhr.onload = function(){
+        if(this.status === 200){
+           let jokes = JSON.parse(this.responseText);
+        }
+
+        const output = `<div class="joke">
+        <div class="index">1</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sunt
+        architecto placeat praesentium explicabo cumque facere cum eum aliquam
+        repellendus.
+      </div>`
+    
+    }
+
+    xhr.send();
 
     e.preventDefault();
 }
