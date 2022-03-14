360df6f0afee071dbdbcde037665dbb606709856
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-14 20:33:27 +0100

    update

--- easyhttp.js
+++ easyhttp.js
@@ -2,12 +2,12 @@ function easyHttp(){
     this.http = new XMLHttpRequest();
 }
 
-easyHttp.prototype.get = function(url){
+easyHttp.prototype.get = function(url, callback){
     this.http.open('GET', url, true);
 
     this.http.onload = function(){
         if(this.status === 200){
-            console.log(this.responseText);      
+            callbackthis.responseText);      
         }     
     }
     this.http.send();
