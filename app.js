59f80810b273353112aa5fa7bad7b06c686078af
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-15 20:28:37 +0100

    update

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ const http = new easyHttp();
 
 http.get('https://jsonplaceholder.typicode.com/posts1', function(error, response){
     if(error){
-        console.log(err);
+        console.log(error);
     } else {
         console.log(response);
     }
