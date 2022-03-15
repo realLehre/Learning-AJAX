c5b554c2562a6a979f49ead0ca038fe0f8bf7616
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-15 20:52:34 +0100

    update

--- app.js
+++ app.js
@@ -106,4 +106,6 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(error, response)
     }    
 });
 
-http.get('https://jsonplaceholder.typicode.com/posts/1');
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
+    
+});
