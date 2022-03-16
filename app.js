f1219bf80ec27a5efa141b64c97ba1142da1831e
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-16 20:59:35 +0100

    update

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
 
 xhr.open('PUT', 'https://jsonplaceholder.typicode.com/posts/1', true)
 
-xhr.setRequestHeader('')
+xhr.setRequestHeader('Content')
 
 xhr.onload = function(data){
     if(this.status === 200){
