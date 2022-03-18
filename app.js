f6c7dcfda37b0e20ad6143edc682523095529eb5
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-18 20:18:31 +0100

    update

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ function getJson(){
 document.getElementById('button3').addEventListener('click', getExternal);
 
 function getExternal(){
-    fetch('https://api.github.com/users')
+    fetch('https://api.github.com/users1')
         .then(res => {
             return res.json();
         })
