a6bbfaab5bae3f85b4873aaaea9721fc6713bf24
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-20 17:03:21 +0100

    update

--- easyhttp2.js
+++ easyhttp2.js
@@ -2,7 +2,7 @@ class Connect{
     get(url){
         fetch(url)
             .then(res => res.json())
-            .then(data => data)
+            .then(data => console.log(data))
             .catch(err => err)
     }
 }
