16cf947d62e87c176309bd6517b75a4b44599799
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-20 17:04:49 +0100

    update

--- easyhttp2.js
+++ easyhttp2.js
@@ -1,6 +1,6 @@
 class Connect{
     get(url){
-       return new Promise((resolve))
+       return new Promise((resolve, reject))
         fetch(url)
             .then(res => res.json())
             .then(data => console.log(data))
