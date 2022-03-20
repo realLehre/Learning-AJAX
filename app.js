ff8922ac2e24797230cf3c5a5feaef58d0a59e45
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-20 20:15:52 +0100

    update

--- app.js
+++ app.js
@@ -284,6 +284,8 @@ async function getUsers(){
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
     const data = response.json();
+
+    return data;
 }
 
-console.log(getUsers());
+getUsers().then(data => console.log(data));
