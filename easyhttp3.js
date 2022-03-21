bb6e8fe5c29051c548ceac11f4dce7b2c2df1552
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-21 21:27:19 +0100

    update

--- easyhttp3.js
+++ easyhttp3.js
@@ -2,8 +2,6 @@ class easyHttp3{
     async get(){
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
-        return response;
-
         console.log(response);
 
         const res = await response.json();
